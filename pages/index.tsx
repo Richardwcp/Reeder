5908d04d775103843ef974fd8bd6eb22f8f9dbf8
Richard Yang <richard@wecodeplus.co.uk>
2020-06-19 18:56:04 +0100

    Added Sass and styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetServerSideProps } from 'next'
 import jsdom from 'jsdom'
+import Header from './components/Header/Header'
+import styles from './index.module.scss'
 
 type Item = {
   title: string
@@ -15,23 +17,42 @@ type HomeProps = {
 export default function Home({ items }: HomeProps) {
   return (
     <>
+      <Header />
       {items.map(({ title, description, link, date }) => {
         return (
-          <article>
-            <h2>
-              <a href={link} target='_blank' rel='noopener'>
-                {title}
-              </a>
-              {description}
-            </h2>
+          <article className={styles.article}>
+            <div className={(styles.example, styles.card)}>
+              <div className={styles.wrapper}>
+                <div className={styles.date}>
+                  <span className={styles.day}>19</span>
+                  <span className={styles.month}>June</span>
+                  <span className={styles.year}>2020</span>
+                </div>
+                <div className={styles.data}>
+                  <div className={styles.content}>
+                    <span className={styles.author}>Jane Doe</span>
+                    <h1 className={styles.title}>
+                      <a href={link}>{title}</a>
+                    </h1>
+                    <p className={styles.text}>{description}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </article>
         )
       })}
+      <style global jsx>{`
+        body {
+          margin: 0;
+          font-family: 'Open Sans';
+        }
+      `}</style>
     </>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async context => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const RSS_URL: string = 'http://feeds.skynews.com/feeds/rss/technology.xml'
   const { JSDOM } = jsdom
   const { window } = new JSDOM(``)
@@ -44,7 +65,7 @@ export const getServerSideProps: GetServerSideProps = async context => {
   const nodeList = doc.querySelectorAll('item')
 
   let items = []
-  nodeList.forEach(el => {
+  nodeList.forEach((el) => {
     const title = el.querySelector('title').innerHTML
     const description = el.querySelector('description').innerHTML
     const link = el.querySelector('link').innerHTML
