ba638d6ad6b53ea290ca1d12d326749dedd2275e
Ahmed AbdulRahman <a.rahman1198@gmail.com>
2020-06-20 14:21:08 +0100

    Auto stash before merge of "styling/style-global" and "origin/styling/style-global"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app'
 import './app.global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
-import { GetServerSideProps } from 'next'
-import jsdom from 'jsdom'
-import Header from './components/Header/header'
-import styles from './index.module.scss'
-import globalStyles from './global.module.scss'
+import { GetServerSideProps } from "next";
+import jsdom from "jsdom";
+import Header from "./components/Header/header";
+import styles from "./index.module.scss";
 
 type Item = {
-  title: string
-  description: string
-  link: string
-  date: string
-}
+  title: string;
+  description: string;
+  link: string;
+  date: string;
+};
 
 type HomeProps = {
-  items: Item[]
-}
+  items: Item[];
+};
 
 export default function Home({ items }: HomeProps) {
   return (
@@ -21,66 +20,64 @@ export default function Home({ items }: HomeProps) {
       <Header />
       {items.map(({ title, description, link, date }) => {
         return (
-          <body>
-            <article className={styles.article}>
-              <div className={(styles.example, styles.card)}>
-                <div className={styles.wrapper}>
-                  <div className={styles.date}>
-                    <span className={styles.day}>19</span>
-                    <span className={styles.month}>June</span>
-                    <span className={styles.year}>2020</span>
-                  </div>
-                  <div className={styles.data}>
-                    <div className={styles.content}>
-                      <span className={styles.author}>Jane Doe</span>
-                      <h1 className={styles.title}>
-                        <a href={link}>{title}</a>
-                      </h1>
-                      <p className={styles.text}>{description}</p>
-                    </div>
+          <article className={styles.article}>
+            <div className={(styles.example, styles.card)}>
+              <div className={styles.wrapper}>
+                <div className={styles.date}>
+                  <span className={styles.day}>19</span>
+                  <span className={styles.month}>June</span>
+                  <span className={styles.year}>2020</span>
+                </div>
+                <div className={styles.data}>
+                  <div className={styles.content}>
+                    <span className={styles.author}>Jane Doe</span>
+                    <h1 className={styles.title}>
+                      <a href={link}>{title}</a>
+                    </h1>
+                    <p className={styles.text}>{description}</p>
                   </div>
                 </div>
               </div>
-            </article>
-          </body>
-        )
+            </div>
+          </article>
+        );
       })}
     </>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const RSS_URL: string = 'http://feeds.skynews.com/feeds/rss/technology.xml'
-  const { JSDOM } = jsdom
-  const { window } = new JSDOM(``)
+  const RSS_URL: string = "http://feeds.skynews.com/feeds/rss/technology.xml";
+  const { JSDOM } = jsdom;
+  const { window } = new JSDOM(``);
 
-  const res = await fetch(RSS_URL)
-  const str = await res.text()
+  const res = await fetch(RSS_URL);
+  const str = await res.text();
 
-  const doc: Document = new window.DOMParser().parseFromString(str, 'text/xml')
+  const doc: Document = new window.DOMParser().parseFromString(str, "text/xml");
 
-  const nodeList = doc.querySelectorAll('item')
+  const nodeList = doc.querySelectorAll("item");
 
-  let items = []
+  let items = [];
   nodeList.forEach((el) => {
-    const title = el.querySelector('title').innerHTML
-    const description = el.querySelector('description').innerHTML
-    const link = el.querySelector('link').innerHTML
-    const date = el.querySelector('pubDate').innerHTML
+    const title = el.querySelector("title").innerHTML;
+    const description = el.querySelector("description").innerHTML;
+    const link = el.querySelector("link").innerHTML;
+    const date = el.querySelector("pubDate").innerHTML;
 
     const item = {
       title,
       description,
       link,
       date,
-    }
+    };
 
-    items.push(item)
-  })
+    items.push(item);
+  });
 
   return {
     props: {
       items,
     },
-  }
-}
+  };
+};
