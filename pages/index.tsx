3fc0e8a809b83f3cd1979c755ac782ce9023b761
Pranjal <pjately078@gmail.com>
2020-06-20 01:40:21 +0100

    index css card structure and style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next'
 import jsdom from 'jsdom'
-import Header from './components/Header/Header'
+import Header from './components/Header/header'
 import styles from './index.module.scss'
 
 type Item = {
@@ -20,39 +20,27 @@ export default function Home({ items }: HomeProps) {
       <Header />
       {items.map(({ title, description, link, date }) => {
         return (
-          <article className={styles.article}>
-            <div className={(styles.example, styles.card)}>
-              <div className={styles.wrapper}>
-                <div className={styles.date}>
-                  <span className={styles.day}>19</span>
-                  <span className={styles.month}>June</span>
-                  <span className={styles.year}>2020</span>
-                </div>
-                <div className={styles.data}>
-                  <div className={styles.content}>
-                    <span className={styles.author}>Jane Doe</span>
-                    <h1 className={styles.title}>
-                      <a href={link}>{title}</a>
-                    </h1>
-                    <p className={styles.text}>{description}</p>
-                  </div>
-                </div>
+          <section className={styles.article}>
+            <article className={styles.card}>
+              <div className={styles.imageContainer}></div>
+              <div className={styles.content}>
+                <p className={styles.headline}>
+                  <a href={link}>{title}</a>
+                </p>
+                <p className={styles.description}>{description}</p>
               </div>
-            </div>
-          </article>
+              <div className={styles.cardBottom}>
+                <p className={styles.author}>Jane Doe</p>
+              </div>
+            </article>
+          </section>
         )
       })}
-      <style global jsx>{`
-        body {
-          margin: 0;
-          font-family: 'Open Sans';
-        }
-      `}</style>
     </>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async context => {
   const RSS_URL: string = 'http://feeds.skynews.com/feeds/rss/technology.xml'
   const { JSDOM } = jsdom
   const { window } = new JSDOM(``)
@@ -65,7 +53,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const nodeList = doc.querySelectorAll('item')
 
   let items = []
-  nodeList.forEach((el) => {
+  nodeList.forEach(el => {
     const title = el.querySelector('title').innerHTML
     const description = el.querySelector('description').innerHTML
     const link = el.querySelector('link').innerHTML
