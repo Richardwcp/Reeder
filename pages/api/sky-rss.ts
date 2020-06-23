2d4d92f4918d15701b26e748f8f42fb3107459d9
Pranjal <pjately078@gmail.com>
2020-06-23 18:43:24 +0100

    get all sky news feeds from db and save posts

--- pages/api/sky-rss.ts
+++ pages/api/sky-rss.ts
@@ -3,10 +3,50 @@ import jsdom from 'jsdom'
 import { client, q } from '@utils/fauna-client'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const authorName = 'Sky News'
+
+  const rssUrls = await getRssFeedsByAuthorName(authorName)
+
+  const promises = rssUrls.map(async url => {
+    const items = await extractRssContent(url)
+    return await saveToDb(items)
+  })
+
+  try {
+    await Promise.all(promises)
+    res.status(200).send('Sky News posts saved to DB')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal server error')
+  }
+}
+
+async function getRssFeedsByAuthorName(name: string) {
+  const { data } = await client.query(
+    q.Map(
+      q.Paginate(
+        q.Join(
+          q.Match(q.Index('author_by_name'), name),
+          q.Index('feeds_by_author')
+        )
+      ),
+      q.Lambda('ref', q.Get(q.Var('ref')))
+    )
+  )
+
+  const rssUrls = data.map(({ data: { url } }) => {
+    return url
+  })
+
+  return rssUrls
+}
+
+async function extractRssContent(url) {
   const { JSDOM } = jsdom
   const { window } = new JSDOM(``)
 
-  const promise = await fetch('http://feeds.skynews.com/feeds/rss/home.xml')
+  const feedRef = await getFeedByUrl(url)
+  const promise = await fetch(url)
   const str = await promise.text()
 
   const doc: Document = new window.DOMParser().parseFromString(str, 'text/xml')
@@ -24,11 +64,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       description,
       link,
       date,
+      rss_feed: feedRef,
     }
 
     items.push(item)
   })
 
+  return items
+}
+
+async function saveToDb(items: Array<any>) {
   try {
     await client.query(
       q.Map(
@@ -42,5 +87,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   } catch (error) {
     console.error(error)
   }
-  res.status(200).json({ items })
+}
+
+async function getFeedByUrl(url: string) {
+  const { ref } = await client.query(
+    q.Get(q.Match(q.Index('feed_by_url'), url))
+  )
+  return ref
 }
