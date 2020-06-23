9b74c23c57580d9138b38ac2d093c8b2732efc10
Richard Yang <richard@wecodeplus.co.uk>
2020-06-23 10:35:11 +0100

    card styling and technology page

---
+++ pages/explore/technology.tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+import styles from './technology.module.scss'
+import { getAllPosts } from '../../lib/posts'
+import Feed from '../components/Feed/feed'
+
+type Post = {
+  title: string
+  description: string
+  link: string
+  date: string
+}
+
+type TechnologyProps = {
+  posts: Post[]
+}
+
+export default function Technology({ posts }: TechnologyProps) {
+  return (
+    <>
+      <nav style={{ width: '100%' }}>
+        <ul
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            listStyle: 'none',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+          }}
+        >
+          <li>
+            <Link href='/explore'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/explore/technology'>
+              <a>Technology</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/explore/entertainment'>
+              <a>Entertainment</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Feed posts={posts} />
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
+}
