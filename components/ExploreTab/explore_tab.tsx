6db918f80d0f4c66ed3d6fbdb978931d3834113b
Richard Yang <richard@wecodeplus.co.uk>
2020-06-24 21:25:36 +0100

    Moved styles to stylesheet and stlyed tabs grayscale

--- components/ExploreTab/explore_tab.tsx
+++ components/ExploreTab/explore_tab.tsx
@@ -8,16 +8,8 @@ interface Props {}
 export default function exploreTab({}: Props) {
   const router = useRouter()
   return (
-    <nav style={{ width: '100%' }}>
-      <ul
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          listStyle: 'none',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}
-      >
+    <nav className={styles.nav}>
+      <ul className={styles.tabList}>
         <li className={router.pathname === '/explore' ? styles.active : ''}>
           <Link href='/explore'>
             <a>Home</a>
