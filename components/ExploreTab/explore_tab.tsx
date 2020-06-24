56c2d242a2431207282699abb11700674725e9e5
Pranjal <pjately078@gmail.com>
2020-06-24 19:26:41 +0100

    active tab

--- components/ExploreTab/explore_tab.tsx
+++ components/ExploreTab/explore_tab.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from './explore_tab.module.scss'
 
 interface Props {}
 
 export default function exploreTab({}: Props) {
+  const router = useRouter()
   return (
     <nav style={{ width: '100%' }}>
       <ul
@@ -15,17 +18,25 @@ export default function exploreTab({}: Props) {
           alignItems: 'center',
         }}
       >
-        <li>
+        <li className={router.pathname === '/explore' ? styles.active : ''}>
           <Link href='/explore'>
             <a>Home</a>
           </Link>
         </li>
-        <li>
+        <li
+          className={
+            router.pathname === '/explore/technology' ? styles.active : ''
+          }
+        >
           <Link href='/explore/technology'>
             <a>Technology</a>
           </Link>
         </li>
-        <li>
+        <li
+          className={
+            router.pathname === '/explore/entertainment' ? styles.active : ''
+          }
+        >
           <Link href='/explore/entertainment'>
             <a>Entertainment</a>
           </Link>
