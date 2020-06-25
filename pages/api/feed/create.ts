745ac8ac63378f4105c8141cb6d116f292db57ff
Pranjal <pjately078@gmail.com>
2020-06-25 20:23:50 +0100

    endpoint to create author and rss_feed in db

--- pages/api/feed/create.ts
+++ pages/api/feed/create.ts
@@ -10,32 +10,35 @@ export default async (
   res: NextApiResponse<Data | string>
 ) => {
   if ('POST' === req.method) {
-    console.log(req.body)
-
     const { author, feedName, feedUrl, category } = req.body
 
-    const authorRef = await client.query(
-      q.Create(q.Collection('author'), { data: { name: author } })
-    )
+    try {
+      const { ref: authorRef } = await client.query(
+        q.Create(q.Collection('author'), { data: { name: author } })
+      )
 
-    const categoryRef = await client.query(
-      q.Get(q.Match(q.Index('category_by_name'), category))
-    )
+      const { ref: categoryRef } = await client.query(
+        q.Get(q.Match(q.Index('category_by_name'), category))
+      )
 
-    const { data } = await client.query(
-      q.Create(q.Collection('rss_feed'), {
-        data: {
-          name: feedName,
-          url: feedUrl,
-          author: authorRef.ref,
-          category: categoryRef.ref,
-        },
+      await client.query(
+        q.Create(q.Collection('rss_feed'), {
+          data: {
+            name: feedName,
+            url: feedUrl,
+            author: authorRef,
+            category: categoryRef,
+          },
+        })
+      )
+      res.status(200).json({ success: true, msg: 'Feed created' })
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        msg: 'Something went wrong, please try again.',
       })
-    )
-    console.log('data', data)
-
-    res.status(200).json({ success: true, msg: 'Feed created' })
-  } else {
-    res.status(404).send('Not found')
+    }
   }
+
+  res.status(404).send('404 Not Found')
 }
