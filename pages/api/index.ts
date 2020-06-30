6487d367edb8798c8d6acef7d67c0be971dc6f39
Pranjal <pjately078@gmail.com>
2020-06-30 14:20:46 +0100

    test endpoint updated

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { connectToDatabase } from '@utils/database'
+import { connectToDatabase } from '@utils/mongodb.utils'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const db = await connectToDatabase()
-    const customers = await db.collection('customers').find({}).toArray()
+    const { _id: categoryId } = await db
+      .collection('category')
+      .findOne({ name: 'Home' })
 
-    res.status(200).json({ customers })
+    console.log('categoryId', categoryId)
+
+    const feeds = await db
+      .collection('rss_feed')
+      .find({ category: categoryId })
+      .toArray()
+
+    res.status(200).json({ feeds })
   } catch (e) {
     res.status(500).json({ error: e.message })
   }
