50d83b1a96ee285d610b51c865a40af33523cd52
Pranjal <pjately078@gmail.com>
2020-06-30 23:10:50 +0100

    remove unused function

--- lib/rss_feed.ts
+++ lib/rss_feed.ts
@@ -1,4 +1,3 @@
-import { client, q } from '@utils/fauna-client.utils'
 import { connectToDatabase } from '@utils/mongodb.utils'
 
 async function getAllRssUrls(): Promise<string[]> {
@@ -6,25 +5,6 @@ async function getAllRssUrls(): Promise<string[]> {
   const data = await db.collection('rss_feed').distinct('url')
 
   return data
-}
-
-async function getRssFeedsByAuthorName(name: string) {
-  const { data } = await client.query(
-    q.Map(
-      q.Paginate(
-        q.Join(
-          q.Match(q.Index('author_by_name'), name),
-          q.Index('feeds_by_author')
-        )
-      ),
-      q.Lambda('ref', q.Get(q.Var('ref')))
-    )
-  )
-  const rssUrls = data.map(({ data: { url } }) => {
-    return url
-  })
-
-  return rssUrls
 }
 
 async function getFeedIdByUrl(url: string) {
