c4eac4eb2640b01a37314f2c2a4808c21d81cc75
Pranjal <pjately078@gmail.com>
2020-07-01 13:10:58 +0100

    mongodb test query

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -4,7 +4,24 @@ import { connectToDatabase } from '@utils/mongodb.utils'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const db = await connectToDatabase()
-    const posts = await db.collection('posts').find({}).toArray()
+    const [categoryId] = await db.collection('category').distinct('_id', {
+      name: 'Home',
+    })
+    console.log('categoryId', categoryId)
+
+    const feeds = await db.collection('rss_feed').distinct('_id', {
+      category_id: categoryId,
+    })
+
+    const posts = await db
+      .collection('posts')
+      .find({
+        rss_feed_id: {
+          $in: feeds,
+        },
+      })
+      .sort({ pubDate: -1 })
+      .toArray()
 
     res.status(200).json(posts)
   } catch (e) {
